9d39a6ac0cbee0103fabd60ea1589eebfbba7a06
mjlaudencia <laudenciamj3@gmail.com>
2023-10-15 20:11:06 +0800

    pr

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'username'     => 'mj',
+        'password'     => '1234567',
+        'database'     => 'ecommerce',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
